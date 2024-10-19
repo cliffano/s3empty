c81c72f109a8902005dfbcd505e52eafea049b0e
Cliffano Subagio <cliffano@gmail.com>
2024-10-19 15:21:57 +1100

    Add command run without permissions. Add command runs with empty buckets. Fix separator display.

--- examples/s3empty-cli.sh
+++ examples/s3empty-cli.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 set -o nounset
 
-echo "\n\n========================================"
+printf "\n\n========================================\n"
 echo "Show help guide: s3empty --help"
 s3empty --help
 
-echo "\n\n========================================"
+printf "\n\n========================================\n"
 echo "Run command with default config file: s3empty"
 s3empty
+
+printf "\n\n========================================\n"
+echo "Run command without permission:"
+echo "s3empty --bucket-name some-bucket"
+AWS_PROFILE=studio-tester s3empty --bucket-name some-bucket
 
 export AWS_DEFAULT_REGION=ap-southeast-2
 ACCOUNT_ID=$(AWS_PROFILE=studio-tester aws sts get-caller-identity --query Account --output text)
 export $(python -m aws_assume_role_lib arn:aws:iam::${ACCOUNT_ID}:role/studio-s3empty --profile studio-tester --env)
 
-echo "\n\n========================================"
+printf "\n\n========================================\n"
 echo "Run command with bucket having versioning enabled:"
 echo "s3empty --bucket-name studio-s3empty-with-versioning"
 s3empty --bucket-name studio-s3empty-with-versioning
 
-echo "\n\n========================================"
+printf "\n\n========================================\n"
+echo "Second run command with bucket having versioning enabled when bucket is already emptied:"
+echo "s3empty --bucket-name studio-s3empty-with-versioning"
+s3empty --bucket-name studio-s3empty-with-versioning
+
+printf "\n\n========================================\n"
 echo "Run command with bucket not having versioning enabled:"
+echo "s3empty --bucket-name studio-s3empty-without-versioning"
+s3empty --bucket-name studio-s3empty-without-versioning
+
+printf "\n\n========================================\n"
+echo "Second run command with bucket not having versioning enabled when bucket is already emptied:"
 echo "s3empty --bucket-name studio-s3empty-without-versioning"
 s3empty --bucket-name studio-s3empty-without-versioning
