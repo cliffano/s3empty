641169cc439e0d7d92b1768080abcb34b87a1dad
Cliffano Subagio <cliffano@gmail.com>
2024-11-24 16:28:06 +1100

    Add more integration test scenarios.

--- tests-integration/test_cli.py
+++ tests-integration/test_cli.py
@@ -58,3 +58,52 @@ class TestCli(unittest.TestCase):
         bucket = s3.Bucket("some-bucket")
         objects = list(bucket.objects.all())
         self.assertEqual(len(objects), 0)
+
+    @mock_aws
+    @patch("s3empty.CFGRW.read", return_value={"bucket_names": ["some-bucket"]})
+    def test_cli_having_successful_deletion_with_conf_file(
+        self, mock_read
+    ):  # pylint: disable=unused-argument
+        s3 = boto3.resource("s3")
+        s3.create_bucket(Bucket="some-bucket")
+
+        s3.Bucket("some-bucket").put_object(Key="some-key1", Body="some-message1")
+        s3.Bucket("some-bucket").put_object(Key="some-key2", Body="some-message2")
+
+        runner = CliRunner()
+        result = runner.invoke(cli, ["--conf-file", "some-s3empty.yaml"])
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn(
+            "[s3empty] INFO Reading configuration file some-s3empty.yaml", result.output
+        )
+        self.assertIn(
+            "[s3empty] INFO Buckets to be emptied: some-bucket", result.output
+        )
+        self.assertIn(
+            "[s3empty] INFO Emptying all objects in bucket some-bucket...",
+            result.output,
+        )
+        self.assertIn("[s3empty] INFO Deleted some-key1", result.output)
+        self.assertIn("[s3empty] INFO Deleted some-key2", result.output)
+        self.assertIn(
+            "[s3empty] INFO Successfully emptied all objects in bucket some-bucket",
+            result.output,
+        )
+
+        bucket = s3.Bucket("some-bucket")
+        objects = list(bucket.objects.all())
+        self.assertEqual(len(objects), 0)
+
+    @mock_aws
+    def test_cli_having_successful_deletion_with_inexisting_bucket(self):
+        runner = CliRunner()
+        result = runner.invoke(
+            cli, ["--bucket-name", "some-inexisting-bucket", "--allow-inexisting"]
+        )
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn(
+            "[s3empty] WARNING Bucket some-inexisting-bucket does not exist",
+            result.output,
+        )
